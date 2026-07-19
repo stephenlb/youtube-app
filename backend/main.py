b7340c5e87dc6af6dd74e21084dd0e1a82d203f1
Stephen Blum <blum.stephen@gmail.com>
2026-07-19 11:05:45 -0700

    cleanup main.py

--- backend/main.py
+++ backend/main.py
@@ -45,20 +45,3 @@ async def upload(
         "original" : original,
         "name" : new_name,
     }
-
-## TODO Upload Videos
-## -> sign URL for S3 direct upload
-## -> store video metadata in DB
-## -> PUT to Queue for procoessing
-## -> Thumbnail
-
-## TODO Fetch Videos
-## -> homepage
-## -> trending - last 7 days
-## -> search
-## -> infinite scroll
-
-## TODO Task Management - Nureddin @Nooreldien
-## TODO Likes API
-## TODO Track views counter
-## TODO 
